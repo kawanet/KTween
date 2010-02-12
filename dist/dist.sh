56bcfa44e85babfa305b5aeebfa862c5291fa0a3
kawanet <u-suke@kawa.net>
2010-02-12 22:57:06 +0000

    updated
    git-svn-id: http://kawanet.googlecode.com/svn/lang/as3/KTween/trunk@48 11bd0188-0a8f-11df-89ef-dd47c58a1d1c

--- dist/dist.sh
+++ dist/dist.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 dest=~/Desktop
-date=`date '+%Y%m%d%H'`
+date=`date '+%Y%m%d'`
 name=KTween-$date
 arc=$dest/$name.tar.gz
 
 [ -e $name ] && exit 1
+[ -d ../bin ] || exit 2
+[ -d ../src ] || exit 3
 
 ln -s .. $name
 tar zcvf $arc `find $name/bin $name/src $name/docs $name/tests -type f | egrep -v '/\.|\.fla$'`
