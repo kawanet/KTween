e6f054c271792f41a86c8948953ea0eb1c0322de
kawanet <u-suke@kawa.net>
2010-02-12 22:44:02 +0000

    something wrong with the svn
    git-svn-id: http://kawanet.googlecode.com/svn/lang/as3/KTween/trunk@45 11bd0188-0a8f-11df-89ef-dd47c58a1d1c

---
+++ dist/dist.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+date=`date '+%Y%m%d%H'`
+name=KTween-$date
+arc=$name.tar.gz
+
+[ -e $name ] && exit 1
+
+ln -s .. $name
+tar zcvf $arc `find $name/bin $name/src $name/docs $name/tests -type f | egrep -v '/\.|\.fla$'`
+/bin/rm -f $name
+ls -l $arc
